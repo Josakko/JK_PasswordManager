b6c6bfb7783d6b8f833de1a46550c32eda47e2a4
Josakko <112590914+Josakko@users.noreply.github.com>
2023-08-29 16:17:02 +0200

    Update dashboard_handler.py

--- handlers/dashboard_handler.py
+++ handlers/dashboard_handler.py
@@ -2,15 +2,17 @@ from dashboard import Dashboard
 
 
 class DashboardHandler:
-    def __init__(self, parent, controller, user_id, user_name, data):
+    def __init__(self, parent, controller, user_id, user_name, data, f):
         self.par = parent
         self.con = controller
         self.user_id = user_id
         self.user_name = user_name
         self.data = data
+        self.f = f
+        
         self.dashboard_frame()
 
     def dashboard_frame(self):
-        frame = Dashboard(self.par, self.con, self.user_id, self.user_name, self.data)
+        frame = Dashboard(self.par, self.con, self.user_id, self.user_name, self.data, self.f)
         frame.grid(row=0, column=0, sticky="nsew")
         frame.tkraise()

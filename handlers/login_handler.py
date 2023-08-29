720df69c6a27d3dc4ff3d5f23b459c3f923f7c69
Josakko <112590914+Josakko@users.noreply.github.com>
2023-08-29 16:17:25 +0200

    Update login_handler.py

--- handlers/login_handler.py
+++ handlers/login_handler.py
@@ -5,6 +5,7 @@ class LoginHandler:
     def __init__(self, parent, controller):
         self.par = parent
         self.con = controller
+        
         self.login_frame()
 
     def login_frame(self):

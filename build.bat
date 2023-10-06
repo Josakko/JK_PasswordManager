5cdeb4a894b59c6954ebf390325effdb96e3f03e
Josakko <112590914+Josakko@users.noreply.github.com>
2023-10-06 15:20:53 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -7,8 +7,9 @@ mkdir build\assets\
 copy assets build\assets\
 
 :: --windows-icon-from-ico=assets\JK.ico
+:: --follow-imports
 
-nuitka main.py --clang --onefile  --enable-plugins=tk-inter --disable-console --clean-cache=all --remove-output --output-dir=build 
+nuitka main.py --clang --onefile  --enable-plugins=tk-inter --standalone --disable-console --clean-cache=all --remove-output --output-dir=build --windows-icon-from-ico=icon.png
 
 echo Compiling finished!
 pause

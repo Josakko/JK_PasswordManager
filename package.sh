af42a69b6d89f0f392094c55ef0b17c19cd46d04
josakko <josakko2358@gmail.com>
2024-08-21 03:17:32 +0200

    rewrote linux packgaing and fuctioning

--- package.sh
+++ package.sh
@@ -5,8 +5,7 @@ set -e
 # TODO
 # write this sciript which is going to call package for both linux and windows
 # write web installer for windows
-# rewrite linux packgaing and fuctioning
-# contenerize compiling
+# contenerize compiling for windows (if "practically" possible)
 # contenerize packaging
 # TODO
 
